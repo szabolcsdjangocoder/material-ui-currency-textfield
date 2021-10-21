c0033f90702cabe7f73be3f901a48211c6997f38
Szabolcs Dombi <szabolcs@djangocoders.com>
2021-10-21 10:44:55 +0300

    componentDidUpdate

--- dist/index.js
+++ dist/index.js
@@ -13410,8 +13410,8 @@ var CurrencyTextField = function (_React$Component) {
       this.autonumeric.remove();
     }
   }, {
-    key: "UNSAFE_componentWillReceiveProps",
-    value: function UNSAFE_componentWillReceiveProps(newProps) {
+    key: "componentDidUpdate",
+    value: function componentDidUpdate(newProps) {
       var isValueChanged = this.props.value !== newProps.value && this.getValue() !== newProps.value;
 
       if (isValueChanged) {
